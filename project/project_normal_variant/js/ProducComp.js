57968229d951ec3bd93d92c8ebe1e7f9a036c68e
alkanaft123@yandex.ru <alkanaft123@yandex.ru>
2021-05-11 17:14:27 +0300

    l6

--- project/project_normal_variant/js/ProducComp.js
+++ project/project_normal_variant/js/ProducComp.js
@@ -1,25 +1,29 @@
 Vue.component('products', {
     data(){
         return {
-            catalogUrl: '/catalogData.json',
+            catalogUrl: '../catalogData.json',
             products: [],
             filtered: [],
-            imgCatalog: 'https://placehold.it/200x150',
+            imgCatalog: 'https://via.placeholder.com/200x150',
         }
     },
     methods: {
-        filter(){
-            let regexp = new RegExp(this.userSearch, 'i');
+        filter(usersearch){
+            let regexp = new RegExp(usersearch, 'i');
             this.filtered = this.products.filter(el => regexp.test(el.product_name));
         }
     },
     mounted(){
-        this.$parent.getJson(`${API + this.catalogUrl}`)
+        // this.$parent.getJson(`${API + this.catalogUrl}`)
+        this.$parent.getJson(`${this.catalogUrl}`)
             .then(data => {
                 for(let el of data){
                     this.products.push(el);
                     this.filtered.push(el);
                 }
+            })
+            .catch (()=>{
+                this.$root.$refs.error.visible = true;
             });
     },
     template: `
@@ -31,14 +35,13 @@ Vue.component('products', {
 Vue.component('product', {
     props: ['product', 'img'],
     template: `
-    <div class="product-item">
-                <img :src="img" alt="Some img">
-                <div class="desc">
-                    <h3>{{product.product_name}}</h3>
-                    <p>{{product.price}}₽</p>
-                         <button class="buy-btn" @click="$root.$refs.cart.addProduct(product)">Купить</button>
-<!-- 2                    <button class="buy-btn" @click="$parent.$parent.$refs.cart.addProduct(product)">Купить</button>-->
-                </div>
+        <div class="product-item">
+            <img :src="img" alt="Some img">
+            <div class="desc">
+                <h3>{{product.product_name}}</h3>
+                <p>{{product.price}}₽</p>
+                        <button class="buy-btn" @click="$root.$refs.cart.addProduct(product)">Купить</button>
             </div>
+        </div>
     `
 });
